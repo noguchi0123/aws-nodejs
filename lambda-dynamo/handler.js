ebfe5602662fc60bd2792eb6c889793c51ce5825
noguchi0123 <ji.strada.jan23@gmail.com>
2017-07-17 01:45:42 +0900

    put item to dynamo

--- lambda-dynamo/handler.js
+++ lambda-dynamo/handler.js
@@ -1,16 +1,33 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB({region: 'us-east-1'});
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  var rand_id = Math.floor(Math.random()*10000);
+    var now = new Date().getTime();
+    var fruits = ['apple', 'orange', 'grape', 'peach', 'rice'];
+    var fruit = fruits[Math.floor(Math.random()*fruits.length)];
+    if(fruit=='apple'){
+        price = 100;
+    }else{
+        price = 300;
+    }
+    var params = {
+    TableName: 'put_json',
+    Item: {
+        'id':   {"N":   String(rand_id)}, //primary keyが同じだとうわがきされる問題
+        'name': {"S":   String(fruit)},
+        'price':{"N":   String(price)},
+        'time': {"S":   String(now)}
+    }
+};
+    dynamodb.putItem(params, function(err, data){
+        if(err){
+            console.log(err, err.stack);
+        }else{
+            console.log(data);
+        }
+    });
+    callback(null, String(price) + fruit );
 };
